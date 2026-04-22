544154982bc9222512ca87ff026da5464f2e0937
Edrel Aquino <edrelaquino02@proton.me>
2026-04-22 15:04:18 +0800

    created model folder and added Gear Model

---
+++ Project_AquinoE/Model/Gear.cs
@@ -0,0 +1,11 @@
+﻿namespace RunningGearTracker_AquinoE.Model
+{
+    public class Gear
+    {
+        public int GearID { get; set; }
+        public string Brand { get; set; }
+        public string ModelName { get; set; }
+        public string Category { get; set; }
+        public string PurchaseDate { get; set; }
+    }
+}
