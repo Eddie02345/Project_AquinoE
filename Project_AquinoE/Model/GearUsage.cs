bd0cdd9d1ce6ea4f782dfa9d12be0e8501fed881
Edrel Aquino <edrelaquino02@proton.me>
2026-04-22 15:12:29 +0800

    Added TrainingSessons & GearUsage model

---
+++ Project_AquinoE/Model/GearUsage.cs
@@ -0,0 +1,12 @@
+﻿namespace RunningGearTracker_AquinoE.Model
+{
+    public class GearUsage
+    {
+        public int UsageID { get; set; }
+        public Gear Gear { get; set; }
+        public int GearID { get; set; }
+        public TrainingSessions TrainingSessions { get; set; }
+        public int SessionID { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ Project_AquinoE/Model/TrainingSessions.cs
@@ -0,0 +1,12 @@
+﻿namespace RunningGearTracker_AquinoE.Model
+{
+    public class TrainingSessions
+    {
+        public int SessionID { get; set; }
+        public string ActivityDate { get; set; }
+        public string Location { get; set; }
+        public double Distance_KM { get; set; }
+        public string Duration { get; set; }
+        public string AvgHeartRate { get; set; }
+    }
+}
